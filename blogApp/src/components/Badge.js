85b875a3453b9433fe7d3950993b8cf0d1470fc3
Bruce Song <recall4056@gmail.com>
2022-04-28 00:47:37 +0800

    Update Badge component

--- blogApp/src/components/Badge.js
+++ blogApp/src/components/Badge.js
@@ -1,4 +1,8 @@
 import React from "react";
-export default function Badge({ color, children }) {
-  return <span class={`badge badge--${color}`}>{children}</span>;
+export default function Badge({ color, children, style, onClick = () => {} }) {
+  return (
+    <span class={`badge badge--${color}`} style={style} onClick={onClick}>
+      {children}
+    </span>
+  );
 }
