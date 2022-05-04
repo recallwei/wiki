295a815d53fa6525a268e6fd8f06ca0c34b3770b
Bruce Song <recall4056@gmail.com>
2022-05-05 00:10:44 +0800

    Fix ssg bug

---
+++ website/docs/front-end/docusaurus/understand-ssg.md
@@ -0,0 +1,5 @@
+---
+title: Understand SSG
+---
+
+<!--TODO publish-->
--- website/src/theme/Layout/index.js
+++ website/src/theme/Layout/index.js
@@ -4,7 +4,7 @@
  * This source code is licensed under the MIT license found in the
  * LICENSE file in the root directory of this source tree.
  */
-import React from "react";
+import React, { useState, useEffect } from "react";
 import clsx from "clsx";
 import ErrorBoundary from "@docusaurus/ErrorBoundary";
 import SkipToContent from "@theme/SkipToContent";
@@ -19,6 +19,7 @@ import {
 } from "@docusaurus/theme-common";
 import ErrorPageContent from "@theme/ErrorPageContent";
 import "./styles.css";
+
 export default function Layout(props) {
   const {
     children,
@@ -28,8 +29,11 @@ export default function Layout(props) {
     title,
     description,
   } = props;
+  const [isHomePage, setIsHomePage] = useState(true);
+  useEffect(() => {
+    setIsHomePage(window.location.pathname !== "/");
+  }, []);
   useKeyboardNavigation();
-  const isHomePage = window.location.pathname === "/";
   return (
     <LayoutProviders>
       <PageMetadata title={title} description={description} />
