963c3b69675ccff2efdf7f8c2af731003050fed9
Bruce Song <recall4056@gmail.com>
2022-05-05 08:46:26 +0800

    Add eslint extension

---
+++ website/.eslintrc.js
@@ -0,0 +1,8 @@
+module.exports = {
+  extends: ["plugin:@docusaurus/recommended"],
+  plugins: ["@docusaurus"],
+  rules: {
+    "@docusaurus/string-literal-i18n-messages": "error",
+    "@docusaurus/no-untranslated-text": "warn",
+  },
+};
