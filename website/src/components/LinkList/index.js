a48bd73910d6321e45ef5da020090548245c831a
Bruce Song <recall4056@gmail.com>
2022-05-09 01:00:39 +0800

    Use <Link/> to improve performance

--- website/src/components/LinkList/index.js
+++ website/src/components/LinkList/index.js
@@ -1,15 +1,16 @@
 import React from "react";
 import styles from "./styles.module.css";
+import Link from "@docusaurus/Link";
 export default function LinkList({ srcList = [], style }) {
   return (
     <div style={style}>
       {srcList.map((item) => {
         return (
           <div key={item.title} className={styles.item}>
-            <a href={item.link} target="_self" rel="noreferrer noopener">
+            <Link to={item.link}>
               <img src={item.src} alt={item.alt} loading="lazy" />
               {item.title}
-            </a>
+            </Link>
           </div>
         );
       })}
