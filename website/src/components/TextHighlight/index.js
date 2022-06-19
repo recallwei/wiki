3839cbd2964ddac5bc3568c5c07d3fdf57b341d4
Bruce Song <recall4056@gmail.com>
2022-06-19 13:18:00 +0800

    Update TH component

--- website/src/components/TextHighlight/index.js
+++ website/src/components/TextHighlight/index.js
@@ -3,7 +3,7 @@ import styles from "./styles.module.css";
 import colorSwitch from "@site/src/util/color-switch.js";
 
 export default function TextHighlight({ bgColor, children }) {
-  const bgStyle = { color: `${colorSwitch(bgColor)}` };
+  const bgStyle = { backgroundColor: `${colorSwitch(bgColor)}` };
   return (
     <span
       className={styles.highlightArea}
