a1907c7fa878e651283d981014f23ee3e4ddb79a
Bruce Song <recall4056@gmail.com>
2022-07-16 23:07:13 +0800

    Add openWindow property for HyperLink Component.

--- website/src/components/HyperLink/index.js
+++ website/src/components/HyperLink/index.js
@@ -3,25 +3,30 @@ import styles from "./styles.module.css";
 import clsx from "clsx";
 import Link from "@docusaurus/Link";
 import LinkSVG from "/static/svg/link.svg";
+import useDocusaurusContext from "@docusaurus/useDocusaurusContext";
 
 export default function HyperLink({
   bold,
   children,
   customStyle,
   link,
+  openWindow = false,
   wrapperClassName,
 }) {
+  const { siteConfig } = useDocusaurusContext();
   return (
     <span
       style={customStyle}
       className={clsx(styles.linkWrapper, wrapperClassName)}
     >
-      <Link to={link}>
+      <Link to={openWindow ? siteConfig.url + link : link}>
         <span className={clsx(styles.textArea, bold && styles.textBold)}>
           {children}
-          <span className={styles.linkSVG}>
-            <LinkSVG />
-          </span>
+          {openWindow && (
+            <span className={styles.linkSVG}>
+              <LinkSVG />
+            </span>
+          )}
         </span>
       </Link>
     </span>
