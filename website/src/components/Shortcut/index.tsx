e791079960f5a672a1939cfa69bfba45124b4366
Bruce Song <recall4056@gmail.com>
2022-07-17 16:42:36 +0800

    Refactor Shortcut Component.

---
+++ website/src/components/Shortcut/index.tsx
@@ -0,0 +1,24 @@
+import React, { CSSProperties, ReactNode } from "react";
+import styles from "./styles.module.css";
+import clsx from "clsx";
+
+type ShortcutProps = {
+  children: ReactNode;
+  wrapperClassName?: string;
+  wrapperStyle: CSSProperties;
+};
+
+export default function Shortcut({
+  children,
+  wrapperClassName,
+  wrapperStyle,
+}: ShortcutProps): JSX.Element {
+  return (
+    <span
+      className={clsx(styles.shortcut, wrapperClassName)}
+      style={wrapperStyle}
+    >
+      {children}
+    </span>
+  );
+}
