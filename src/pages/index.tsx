d22b84d54fc8ad1d5e95ec25394798cab5c1600b
Bruce Song <recall4056@gmail.com>
2022-08-12 12:14:18 +0800

    Update.

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -40,7 +40,6 @@ type ContactMeBtnProps = {
 };
 
 export default function Home(): JSX.Element {
-  const PORTFOLIO = "Portfolio";
   return (
     <BrowserOnly fallback={undefined}>
       {() => {
@@ -61,60 +60,7 @@ export default function Home(): JSX.Element {
                   {backendMenuData.backend}
                 </div>
                 <GridList data={backendGridList} />
-                <div className={styles.portfolioArea}>
-                  <div className={styles.listTitle}>{PORTFOLIO}</div>
-                  <div className={styles.portfolioListWrapper}>
-                    {portfolioListData.map((item: PortfolioType) => {
-                      return (
-                        <Link
-                          className={styles.portfolioCardWrapper}
-                          key={item.name}
-                          to={item.siteLink}
-                        >
-                          <div className={styles.portfolioCardLeft}>
-                            <h4>{item.name}</h4>
-                            <div className={styles.description}>
-                              {item.description}
-                            </div>
-                            {!isMobileDevice && (
-                              <div className={styles.tagWrapper}>
-                                {item.stackTags?.map((tag: TagType) => {
-                                  return (
-                                    <div
-                                      className={styles.tag}
-                                      style={{ background: tag.bgColor }}
-                                    >
-                                      <img
-                                        src={tag.iconSrc}
-                                        loading="eager"
-                                        width="12"
-                                        height="12"
-                                      />
-                                      {tag.name}
-                                    </div>
-                                  );
-                                })}
-                              </div>
-                            )}
-                          </div>
-                          <div className={styles.portfolioCardRight}>
-                            <img
-                              src={item.src}
-                              loading="eager"
-                              width="40"
-                              height="40"
-                            />
-                            {!isMobileDevice && (
-                              <div className={clsx(styles.status, styles.tag)}>
-                                {item.status}
-                              </div>
-                            )}
-                          </div>
-                        </Link>
-                      );
-                    })}
-                  </div>
-                </div>
+                <PortfolioArea isMobileDevice={isMobileDevice} />
               </div>
             </main>
           </Layout>
@@ -253,5 +199,58 @@ function ArrowDownBtn(): JSX.Element {
         <path d="M207.029 381.476L12.686 187.132c-9.373-9.373-9.373-24.569 0-33.941l22.667-22.667c9.357-9.357 24.522-9.375 33.901-.04L224 284.505l154.745-154.021c9.379-9.335 24.544-9.317 33.901.04l22.667 22.667c9.373 9.373 9.373 24.569 0 33.941L240.971 381.476c-9.373 9.372-24.569 9.372-33.942 0z"></path>
       </svg>
     </span>
+  );
+}
+
+function PortfolioArea({ isMobileDevice }: HomepageHeaderProps): JSX.Element {
+  const PORTFOLIO = "Portfolio";
+  return (
+    <div className={styles.portfolioArea}>
+      <div className={styles.listTitle}>{PORTFOLIO}</div>
+      <div className={styles.portfolioListWrapper}>
+        {portfolioListData.map((item: PortfolioType) => {
+          return (
+            <Link
+              className={styles.portfolioCardWrapper}
+              key={item.name}
+              to={item.siteLink}
+            >
+              <div className={styles.portfolioCardLeft}>
+                <h4>{item.name}</h4>
+                <div className={styles.description}>{item.description}</div>
+                {!isMobileDevice && (
+                  <div className={styles.tagWrapper}>
+                    {item.stackTags?.map((tag: TagType) => {
+                      return (
+                        <div
+                          className={styles.tag}
+                          style={{ background: tag.bgColor }}
+                        >
+                          <img
+                            src={tag.iconSrc}
+                            loading="eager"
+                            width="12"
+                            height="12"
+                          />
+                          {tag.name}
+                        </div>
+                      );
+                    })}
+                  </div>
+                )}
+              </div>
+              <div className={styles.portfolioCardRight}>
+                <img src={item.src} loading="eager" width="40" height="40" />
+                {!isMobileDevice && (
+                  <div className={clsx(styles.status, styles.tag)}>
+                    {item.status}
+                  </div>
+                )}
+              </div>
+            </Link>
+          );
+        })}
+      </div>
+    </div>
   );
 }
